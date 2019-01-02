948d39448c50bc043b17f8b1d66438f41b62f393
Bobby Gammill <bgammill@gmail.com>
2019-01-01 17:35:14 -0700

    Initial commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace NotificationThingy
+{
+    [Activity(Label = "Notification Thingy", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        public static TextView textView;
+
+        void CreateNotificationChannel()
+        {
+            var name = Resources.GetString(Resource.String.channel_name);
+            var description = GetString(Resource.String.channel_description);
+            var id = GetString(Resource.String.channel_id);
+            var channel = new NotificationChannel(id, name, NotificationImportance.Default)
+            {
+                Name = name,
+                Description = description,
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            CreateNotificationChannel();
+
+            textView = FindViewById<TextView>(Resource.Id.textView1);
+
+            var startButton = FindViewById<Button>(Resource.Id.startButton);
+            startButton.Click += delegate
+            {
+                textView.Text = "connecting";
+
+                var intent = new Intent(this, typeof(MqttService));
+                StartService(intent);
+            };
+
+            var stopButton = FindViewById<Button>(Resource.Id.stopButton);
+            stopButton.Click += delegate
+            {
+                var intent = new Intent(this, typeof(MqttService));
+                StopService(intent);
+            };
+        }
+    }
+}
+
---
+++ App1/MqttService.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json;
+
+namespace NotificationThingy
+{
+    [Service]
+    class MqttService : Service
+    {
+        IMqttClient client;
+
+        public override void OnCreate()
+        {
+            var manifestUrl = GetString(Resource.String.manifest_url);
+            var manifestClient = new WebClient();
+            var manifestRaw = manifestClient.DownloadString(manifestUrl);
+
+            var definition = new { host = "", port = 0 };
+            var manifestJson = JsonConvert.DeserializeAnonymousType(manifestRaw, definition);
+
+            var address = manifestJson.host;
+            var port = manifestJson.port;
+
+            var factory = new MqttFactory();
+            client = factory.CreateMqttClient();
+            client.ApplicationMessageReceived += Client_ApplicationMessageReceived;
+            client.Connected += Client_Connected;
+            client.Disconnected += Client_Disconnected;
+
+            var clientId = Guid.NewGuid().ToString();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(address, port)
+                .WithClientId(clientId)
+                .Build();
+
+            client.ConnectAsync(options);
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            return StartCommandResult.Sticky;
+        }
+
+        public override void OnDestroy()
+        {
+            client.DisconnectAsync();
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Client_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            using (var h = new Handler(Looper.MainLooper))
+            {
+                h.Post(() =>
+                {
+                    MainActivity.textView.Text = "disconnected";
+                });
+            }
+        }
+
+        private async void Client_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            var topic = GetString(Resource.String.topic);
+            await client.SubscribeAsync(new TopicFilterBuilder().WithTopic(topic).Build());
+
+            using (var h = new Handler(Looper.MainLooper))
+            {
+                h.Post(() =>
+                {
+                    MainActivity.textView.Text = "connected";
+                });
+            }
+        }
+
+        private void Client_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            using (var h = new Handler(Looper.MainLooper))
+            {
+                h.Post(() =>
+                {
+                    var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    SendNotification(message);
+                });
+            }
+        }
+
+        void SendNotification(string message)
+        {
+            var appName = GetString(Resource.String.app_name);
+
+            var id = GetString(Resource.String.channel_id);
+
+            var builder = new Notification.Builder(Application.Context, id)
+                .SetSmallIcon(Resource.Mipmap.Icon)
+                .SetContentTitle(appName)
+                .SetContentText(message);
+
+            var notification = builder.Build();
+
+            var notificationManager = Application.Context.GetSystemService(Context.NotificationService) as NotificationManager;
+
+            var rnd = new Random();
+            var notificationId = rnd.Next(int.MinValue, int.MaxValue);
+            notificationManager.Notify(notificationId, notification);
+        }
+    }
+}
